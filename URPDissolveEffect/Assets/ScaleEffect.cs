632cfba9b7640ef804944acd92752b7388f08286
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2021-05-26 19:28:45 +0530

    Scaling Script

---
+++ URPDissolveEffect/Assets/ScaleEffect.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleEffect : MonoBehaviour
+{
+   
+    [SerializeField] float scaleValue=0f;
+    [SerializeField] float maxScaleValue;
+    [SerializeField] float scaleSpeed=10f;
+    
+    bool initiateScaleSequence = false;
+    bool initiateDescaleSequence = false;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        
+        if(initiateScaleSequence)
+        {
+            Scale();
+        }
+
+        if(initiateDescaleSequence)
+        {
+            Descale();
+        }
+    }
+
+    void Scale()
+    {
+            if(scaleValue<=maxScaleValue)
+            {
+                scaleValue += Time.deltaTime * scaleSpeed;
+                transform.localScale += new Vector3(scaleValue, scaleValue, scaleValue);
+            }
+            else
+            {
+                initiateScaleSequence = false;
+            }
+    }
+
+    void Descale()
+    {
+            if(scaleValue>=0)
+            {
+                scaleValue -= Time.deltaTime * scaleSpeed;
+                transform.localScale -= new Vector3(scaleValue, scaleValue, scaleValue);
+            }
+            else
+            {
+                initiateDescaleSequence = false;
+            }
+    }
+
+    public void InitiateScaling()
+    {
+        initiateScaleSequence = true;
+    }
+
+    public void InitiateDescaling()
+    {
+        initiateDescaleSequence = true;
+    }
+}
